0445cf3e0748b6206b67dd6798a4e7009bf0c4dd
Saúl Arteaga <alojera15@gmail.com>
2023-10-24 18:13:26 +0100

    refactor: made a component ScrollviewComponent and import it

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Button, Pressable, ScrollView, StyleSheet, Text, View, Image } from 'react-native';
 import QRCode from 'react-native-qrcode-svg';
+import ScrollviewComponent from './components/ScrollviewComponent';
 
 export default function App() {
   const [displayMyQR, setDisplayMyQR] = useState(true);
@@ -9,52 +10,39 @@ export default function App() {
       <View style={styles.topContainer}>
         <Text style={styles.firsttoprowContainer}>My Portfolio App</Text>
         <View style={styles.rowTopSecondContainer}>
-        <Pressable style={styles.buttonruta} onPress={() => setDisplayMyQR(true)}>
-          <Text style={{...{color: 'white', fontWeight: 'bold', textTransform: 'uppercase'}, ...styles.shadoxboxing}}>Mi info</Text>
-        </Pressable>
-        <Button onPress={() => setDisplayMyQR(false)} title="Mi Repo" color="light-gray" accessibilityLabel='Un botón pal QR' />
+          <Pressable style={styles.buttonruta} onPress={() => setDisplayMyQR(true)}>
+            <Text style={{...{color: 'white', fontWeight: 'bold', textTransform: 'uppercase'}, ...styles.shadoxboxing}}>Mi info</Text>
+          </Pressable>
+          <Button onPress={() => setDisplayMyQR(false)} title="Mi Repo" color="light-gray" accessibilityLabel='Un botón pal QR' />
         </View>
       </View>
-      {
+      { 
         displayMyQR ?
           <View style={styles.bodystails}>
             <View>
               <View style={{flexDirection: 'row', alignItems: 'center'}}>
-              <Image style={styles.avatar} source={require('./assets/SofyanAmrabat.jpg')}></Image>
-              <View style={{margin: 10, backgroundColor: 'lightgray', padding: 10, borderRadius: 10, width: '70%'}}>
-            <Text style={{textAlign:'center', fontWeight: '700', fontSize: 20}}>
-            Descripción sobre mí!
-            </Text>
-            <Text>
-              Soy profe y me gusta mi trabajo aunque a veces me de por enrevesar prácticas para mis queridos alumnos
-            </Text>
-              </View>
+                <Image style={styles.avatar} source={require('./assets/SofyanAmrabat.jpg')}></Image>
+                <View style={{margin: 10, backgroundColor: 'lightgray', padding: 10, borderRadius: 10, width: '70%'}}>
+                  <Text style={{textAlign:'center', fontWeight: '700', fontSize: 20}}>
+                  Descripción sobre mí!
+                  </Text>
+                  <Text>
+                    Soy profe y me gusta mi trabajo aunque a veces me de por enrevesar prácticas para mis queridos alumnos
+                  </Text>
+                </View>
               </View>
             <Text style= {{color: 'beriblak', fontWeight: "900", textTransform: 'capitalize', fontSize: 20, textAlign: 'center'}}>
               cosas que me gustan mucho:
             </Text>
-            <ScrollView style={{padding: 10}}>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Salir a pasear</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Senderismo</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Ir a la playita</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Domingos de misa</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>La guitarrita</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>El monte con lluvia</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Viajar</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Música variadita</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Anime</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Ducharme</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Videojuegos</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Ir de cenar romántica</Text>
-            </ScrollView>
+              <ScrollviewComponent/>
             </View>
           </View>
           :
-              <View style={styles.bodystails}>
-            <View style={styles.CentrarcodigoQR}>
-          <QRCode value="https://github.com/adhernea" />
-            </View>
+            <View style={styles.bodystails}>
+              <View style={styles.qrCodeStyle}>
+                <QRCode value="https://github.com/adhernea" />
               </View>
+            </View>
       }
     </View>
   );
@@ -101,7 +89,7 @@ const styles = StyleSheet.create({
     width: 90,
     borderRadius: 100
   },
-  cosasQmeGustanMuxoEstails: {
+  textStyles: {
     borderColor: 'black',
     borderWidth: 1,
     borderStyle: 'dashed',
@@ -113,7 +101,7 @@ const styles = StyleSheet.create({
     fontSize: 16,
     backgroundColor: 'silver'
   },
-  CentrarcodigoQR: {
+  qrCodeStyle: {
     justifyContent: 'center',
     borderWidth: 1,
     width: '100%',
---
+++ components/ScrollviewComponent.tsx
@@ -0,0 +1,40 @@
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ScrollviewComponent = () => {
+  return (
+    <View>
+        <ScrollView style={{padding: 10}}>
+            <Text style={styles.textStyles}>Salir a pasear</Text>
+            <Text style={styles.textStyles}>Senderismo</Text>
+            <Text style={styles.textStyles}>Ir a la playita</Text>
+            <Text style={styles.textStyles}>Domingos de misa</Text>
+            <Text style={styles.textStyles}>La guitarrita</Text>
+            <Text style={styles.textStyles}>El monte con lluvia</Text>
+            <Text style={styles.textStyles}>Viajar</Text>
+            <Text style={styles.textStyles}>Música variadita</Text>
+            <Text style={styles.textStyles}>Anime</Text>
+            <Text style={styles.textStyles}>Ducharme</Text>
+            <Text style={styles.textStyles}>Videojuegos</Text>
+            <Text style={styles.textStyles}>Ir de cenar romántica</Text>
+        </ScrollView>
+    </View>
+  )
+}
+
+export default ScrollviewComponent
+
+const styles = StyleSheet.create({
+    textStyles: {
+        borderColor: 'black',
+        borderWidth: 1,
+        borderStyle: 'dashed',
+        padding: 20,
+        color: 'darkred',
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontStyle: 'italic',
+        fontSize: 16,
+        backgroundColor: 'silver'
+      }
+})
